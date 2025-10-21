c8850a2b67f6c7799acc637aceea33d484a828b0
Devan Cakra Mudra Wijaya <devancakra6@gmail.com>
2025-10-21 20:02:31 +0700

    Update: server.js

--- Src/dashboard-iot/server.js
+++ Src/dashboard-iot/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 const client = mqtt.connect('mqtt://broker.emqx.io');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
